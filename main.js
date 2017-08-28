2567ff5ab64a26cd61acbb671373dcd864d6d51d
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-08-28 10:54:19 -0700

    Changes

--- main.js
+++ main.js
@@ -12,7 +12,13 @@ let result2 = items.map((string, index) => {
     return string.toUpperCase() + (index)
 })
 
+// A higher order function takes a callback function as an argument
+const example = function higherOrderFunc(callbackFunc) {
+    return callbackFunc
+}
+
 console.log('Result', result)
 console.log('Result2', result2)
+console.log('typeof example', typeof example)
 
 
